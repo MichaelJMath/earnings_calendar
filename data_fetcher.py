453ffb3de6a2b6ed4f33a6b5a7fcb7f396556efe
Mike Matthews <michaeljmath@gmail.com>
2018-09-09 15:32:42 -0400

    ENH: Move API_KEY retrieval inside CompanyClient
    Removes the need to pass in the api key on instantiation.

--- data_fetcher.py
+++ data_fetcher.py
@@ -3,14 +3,16 @@
 import os
 
 import numpy as np
+import pandas as pd
 import pycharts
 
 
 class CompanyClient(pycharts.CompanyClient):
     """Child Class of pycharts Company Client that adds additional methods
     to make it more convenient to get data from the YCharts API"""
-    def __init__(self, ycharts_api_key, symbol_list):
-        super(CompanyClient, self).__init__(ycharts_api_key)
+    def __init__(self, symbol_list):
+        YCHARTS_API_KEY = os.environ['YCHARTS_API_KEY']
+        super(CompanyClient, self).__init__(YCHARTS_API_KEY)
         if isinstance(symbol_list, basestring):
             symbol_list = [symbol_list]
         self.symbol_list = map(str.upper, symbol_list)
@@ -46,13 +48,17 @@ class CompanyClient(pycharts.CompanyClient):
                 data_dict[symbol] = {field:np.nan for field in field_list}
         return data_dict
 
+    def to_dataframe(self, data_dict):
+        """Convert data_dict5 from fetch data to a DataFrame"""
+        return pd.DataFrame.from_dict(data_dict, orient='index')
 
 if __name__ == '__main__':
-    YCHARTS_API_KEY = os.environ['YCHARTS_API_KEY']
+
     symbol_list = ['AAPL', 'xyz']
     field_list = ['price', 'average_volume_30']
-    cc = CompanyClient(YCHARTS_API_KEY, 'aapl')
+    cc = CompanyClient( symbol_list)
 
     from pprint import pprint
-    pprint(cc.fetch_data(field_list))
-    # pprint(cc.get_points(cc.symbol_list, field_list))
+    data = cc.fetch_data(field_list)
+    print cc.to_dataframe(data)
+
